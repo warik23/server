cdddeb3742a8215cb38d94f85e55903b4eebd6a2
warik23 <ggamerek123@gmail.com>
2024-11-08 13:36:14 +0100

    server

--- configDB.js
+++ configDB.js
@@ -1,7 +1,20 @@
-export const configDB={
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const configDB_={
     host:'localhost',
     user:'root',
     password:'',
     database:'todos',
+    multipleStatements:true//Engedélyezzük a több utasítást.
+}
+
+export const configDB={
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    port:process.env.DB_PORT,
     multipleStatements:true//Engedélyezzük a több utasítást.
 }
